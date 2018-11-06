5f3d160610fd1554de15c42bf22c6b906cf7dc3d
zb <1826992383@qq.com>
2018-11-06 23:04:28 +0800

    分类页完成

---
+++ js/category.js
@@ -0,0 +1,42 @@
+$(function () {
+  $.ajax({
+    type: 'get',
+    url: 'http://127.0.0.1:9090/api/getcategorytitle',
+    dataType: 'json',
+    success: function (info) {
+      // console.log(info);
+      // 将模板和对象绑定
+      var htmlStr = template('firstTpl', info);
+      //渲染数据到页面上
+      $('.category_nav .first_list').html(htmlStr);
+    }
+  })
+
+  //点击每个li里面的a，使下面的盒子显示
+  // 注册事件委托
+  $('.category_nav .first_list').on('click', '.dropDown', function () {
+    //获取分类标题的id
+    var titleid = $(this).data('titleid');
+    // 切换二级分类显示与隐藏
+    $(this).next().toggleClass('active');
+    //其他的二级分类隐藏
+    $(this).parent().siblings().find('.second_list').removeClass('active');
+
+    $.ajax({
+      type: 'get',
+      url: 'http://127.0.0.1:9090/api/getcategory',
+      data: {
+        titleid: titleid
+      },
+      dataType: 'json',
+      success: function (info) {
+        // console.log(info);
+        // 将对象跟模板绑定
+        var htmlStr2 = template ('secondTpl',info);
+        //将数据渲染到页面上
+        $('.second_list').html(htmlStr2);
+      }
+    })
+
+  })
+})
---
+++ js/index.js
@@ -0,0 +1,34 @@
+$(function () {
+  // 一进入页面先渲染菜单页
+  $.ajax({
+    type: 'get',
+    url: 'http://127.0.0.1:9090/api/getindexmenu',
+    dataType: 'json',
+    success: function (info) {
+      console.log(info);
+      //将模板跟对象绑定
+      var htmlStr1 = template('menuTpl', info);
+      //将数据渲染到页面上
+      $('nav .menu').html(htmlStr1);
+    }
+  })
+
+  // 再渲染折扣产品栏
+  $.ajax({
+    type: 'get',
+    url: 'http://127.0.0.1:9090/api/getmoneyctrl',
+    dataType: 'json',
+    success: function (info) {
+      console.log(info);
+      //将模板与对象绑定
+      var htmlStr2 = template ('productTpl',info);
+      //将数据渲染到页面上
+      $('.sale_main .sale_product').html(htmlStr2);
+    }
+  })
+  
+  //点击更多按钮，切换下面四个li的显示状态  需要注册事件委托
+  $('nav .menu').on('click','.click',function(){
+    $(this).nextAll().toggleClass('active');
+  })
+})
