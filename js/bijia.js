25b6100c9aae869739af5bc47d1aac4df44c25e7
zb <1826992383@qq.com>
2018-11-08 11:42:04 +0800

    比价页面完成

--- js/bijia.js
+++ js/bijia.js
@@ -2,6 +2,7 @@ $(function () {
   //获取地址栏的传参
   var productId = getSearch('productId');
   var categoryName = getSearch('categoryName');
+  // 渲染商品详情页
   $.ajax({
     type: 'get',
     url: 'http://127.0.0.1:9090/api/getproduct',
@@ -11,11 +12,17 @@ $(function () {
     dataType: 'json',
     success: function (info) {
       console.log(info);
+      // 给info 添加一个属性
+      info.categoryname = categoryName;
+
+      var htmlStr = template('productDetail_Tpl', info);
+      $('.product').html(htmlStr);
+
 
     }
   })
 
-
+  //渲染评论页
   $.ajax({
     type: 'get',
     url: 'http://127.0.0.1:9090/api/getproductcom',
@@ -25,6 +32,8 @@ $(function () {
     dataType: 'json',
     success: function (info) {
       console.log(info);
+      var htmlStr2 = template('commentTpl', info);
+      $('.comment_content').html(htmlStr2);
 
     }
   })
